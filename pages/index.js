bcb7512732a3f25d421bc86adedbaeaa3d62358b
Tomáš Bokor <84672201+conway-hash@users.noreply.github.com>
2024-05-04 14:29:44 +0200

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default function Main() {
   return (
     <>
       <Head>
-        <title>CONWAY_HASH$</title>
+        <title>$CONWAY_HASH</title>
         <meta name="description" content="Portfolio site of Tomáš Bokor (Conway-Hash). This site contains informations about me, projects that i created, experience with programming languages, frameworks, tools, and how to contact me." />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
