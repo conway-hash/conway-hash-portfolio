7b7b9a5d63ec732aa7e9064cc34e1a55264296f3
Tomáš Bokor <84672201+conway-hash@users.noreply.github.com>
2024-05-04 13:45:46 +0200

    Update 4projects_main.jsx

--- components/4projects_main.jsx
+++ components/4projects_main.jsx
@@ -10,6 +10,26 @@ const projects_main = forwardRef((props, ref) => {
                 <a href="/#projects" className={styles.button}>Back to home</a>
             </div>
             <div className={`${styles.grid} ${utilities.casual_container} ${props.prop ? utilities.casual_show : utilities.casual_hide}`}>
+                <a className={styles.grid_el} href='https://habito-one.vercel.app/'>
+                    <div className={styles.grid_el_overlay}></div>
+                    <h2>Habitō test v0 (Desktop/Mobile)</h2>
+                    <img src='media/projects/habito0.png' />
+                </a>
+                <a className={styles.grid_el} href='https://habito-app.vercel.app/'>
+                    <div className={styles.grid_el_overlay}></div>
+                    <h2>Habitō test v1 (Mobile)</h2>
+                    <img src='media/projects/habito1.png' />
+                </a>
+                <a className={styles.grid_el} href='https://zdraver.sk/'>
+                    <div className={styles.grid_el_overlay}></div>
+                    <h2>Zdraver.sk</h2>
+                    <img src='media/projects/zdraver.png' />
+                </a>
+                <a className={styles.grid_el} href=''>
+                    <div className={styles.grid_el_overlay}></div>
+                    <h2>Elimo.sk</h2>
+                    <img src='media/projects/elimo.PNG' />
+                </a>
                 {/*
                 <a className={styles.grid_el} href='http://elimo.sk/'>
                     <div className={styles.grid_el_overlay}></div>
