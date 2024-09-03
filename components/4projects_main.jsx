b806332961a4b648fe15e5877615aa92c47d9b0d
Tomáš Bokor <84672201+conway-hash@users.noreply.github.com>
2024-09-03 19:41:16 +0200

    Update 4projects_main.jsx

--- components/4projects_main.jsx
+++ components/4projects_main.jsx
@@ -11,15 +11,15 @@ const projects_main = forwardRef((props, ref) => {
                 <Link href="/#projects" aria-label="projects" className={styles.button}>Back to home</Link>
             </div>
             <div className={`${styles.grid} ${utilities.casual_container} ${props.prop ? utilities.casual_show : utilities.casual_hide}`}>
-                <a className={styles.grid_el} href='https://habito-one.vercel.app/'>
+                <a className={styles.grid_el} href='https://expenses-notebook-notex.vercel.app/'>
                     <div className={styles.grid_el_overlay}></div>
-                    <h2>HabitōTestV0 (Dskt/Mob)</h2>
-                    <img src='media/projects/habito0.png' alt='HabitoV0' />
+                    <h2>Notex (in development)</h2>
+                    <img src='media/projects/notex.png' alt='Notex' />
                 </a>
                 <a className={styles.grid_el} href='https://habito-app.vercel.app/'>
                     <div className={styles.grid_el_overlay}></div>
-                    <h2>HabitōTestV1 (Mob)</h2>
-                    <img src='media/projects/habito1.png' alt='HabitoV1' />
+                    <h2>Habitō (in development)</h2>
+                    <img src='media/projects/habito1.png' alt='Habito' />
                 </a>
                 <a className={styles.grid_el} href='https://zdraver.sk/'>
                     <div className={styles.grid_el_overlay}></div>
